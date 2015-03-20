45ffda6ddc40b1bde9c76d4f9a7799527156e66e
Thomas Papillon <tp@irrealis.me>
2015-03-20 17:11:39 +0100

    Change secret key

--- blackballot/settings.py
+++ blackballot/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5^oo2y53naax6t48x@h*b7ogh33phz*2rklo=6dhb7&+*-1$46'
+SECRET_KEY = 'CHANGEME'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
